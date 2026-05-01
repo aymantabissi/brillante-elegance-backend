4d3c8f0700f59e0b30ec70d12ff0f966767f7041
aymantabissi <aymanraisse7@gmail.com>
2026-05-01 20:16:59 +0200

    update cors

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const __dirname  = path.dirname(__filename)
 const app = express()
 
 app.use(cors({
-  origin: '*',
+  origin: function(origin, callback) {
+    callback(null, true)
+  },
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
